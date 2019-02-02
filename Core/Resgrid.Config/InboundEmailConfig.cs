4e1b5654a8cbb611f5a3a0c7741dd571d89fc020
Shawn Jackson <shawn@resgrid.com>
2019-02-02 07:19:06 -0800

    Added inbound email settings to Config project.

---
+++ Core/Resgrid.Config/InboundEmailConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace Resgrid.Config
+{
+	public static class InboundEmailConfig
+	{
+		public static string DispatchDomain = "d.yourcompany.local";
+		public static string DispatchTestDomain = "testd.yourcompany.local";
+
+		public static string ListsDomain = "l.yourcompany.local";
+		public static string ListsTestDomain = "testl.yourcompany.local";
+
+		public static string GroupsDomain = "g.yourcompany.local";
+		public static string GroupsTestDomain = "testg.yourcompany.local";
+
+		public static string GroupMessageDomain = "gm.yourcompany.local";
+		public static string GroupTestMessageDomain = "testgm.yourcompany.local";
+	}
+}

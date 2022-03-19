e02d1830ad793eb7cc6d84eefc0e59306ad5f6ab
Shawn Jackson <shawn@resgrid.com>
2022-03-19 10:16:51 -0700

    docker data folder structure

---
+++ Docker/db/create-databases.sql
@@ -0,0 +1,6 @@
+﻿CREATE DATABASE Resgrid;
+GO
+CREATE DATABASE ResgridWorkers;
+GO
+CREATE DATABASE ResgridOIDC;
+GO
--- Docker/db/run-initialization.sh
+++ Docker/db/run-initialization.sh
@@ -3,4 +3,4 @@ sleep 90s
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Resgrid123! -d master -i create-databases.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Resgrid123! -d master -i create-database.sql

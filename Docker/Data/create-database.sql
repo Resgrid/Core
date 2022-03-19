c51e54d305419f752ccd3b9af9676fa34a69cd16
Shawn Jackkson <shawn@resgrid.com>
2022-03-19 08:03:15 -0700

    Docker compose updates

--- Docker/Data/create-database.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE DATABASE Resgrid;
-GO
-CREATE DATABASE ResgridWorkers;
-GO
-CREATE DATABASE ResgridOIDC;
-GO
---
+++ Docker/data/create-databases.sql
@@ -0,0 +1,17 @@
+﻿IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'Resgrid')
+    BEGIN
+        CREATE DATABASE Resgrid;
+    END
+GO
+
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'ResgridWorkers')
+    BEGIN
+        CREATE DATABASE ResgridWorkers;
+    END
+GO
+
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'ResgridOIDC')
+    BEGIN
+        CREATE DATABASE ResgridOIDC;
+    END
+GO

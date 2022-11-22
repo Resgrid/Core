bbd110659b88a6e90a2a4da8163fff6520be0c7b
Shawn Jackson <shawn@resgrid.com>
2022-11-21 22:00:43 -0800

    Fixing build by updating the config project. Fixing solution build by adding NoSqlProject.

--- Core/Resgrid.Config/DataConfig.cs
+++ Core/Resgrid.Config/DataConfig.cs
@@ -4,6 +4,10 @@
 	{
 		public static string ConnectionString = "Server=rgdevserver;Database=Resgrid;User Id=resgrid_app;Password=resgrid123;MultipleActiveResultSets=True;";
 
+		public static string NoSqlConnectionString = "mongodb://resgrid:resgrid123@rgdevserver:27017";
+		public static string NoSqlDatabaseName = "resgrid";
+		public static string NoSqlApplicationName = "Resgrid";
+
 		public const string UsersIdentityRoleId = "38b461d7-e848-46ef-8c06-ece5b618d9d1";
 		public const string AdminsIdentityRoleId = "1f6a03a8-62f4-4179-80fc-2eb96266cf04";
 		public const string AffiliatesIdentityRoleId = "3aba8863-e46d-40cc-ab86-309f9c3e4f97";
--- Core/Resgrid.Config/InfoConfig.cs
+++ Core/Resgrid.Config/InfoConfig.cs
@@ -3,5 +3,23 @@
 	public static class InfoConfig
 	{
 		public static int ConfigVersion = 0;
+
+		public static string UnitAppKey = "UnitAppKey";
+
+		public static string ResponderAppKey = "ResponderAppKey";
+
+		public static string DispatchAppKey = "DispatchAppKey";
+
+		public static string BigBoardKey = "BigBoardKey";
+
+		public static string ICAppKey = "ICAppKey";
+
+		public static string ApiKey = "ApiKey";
+
+		public static string WebsiteKey = "WebsiteKey";
+
+		public static string RelayAppKey = "RelayAppKey";
+
+		public static string EmailProcessorKey = "EmailProcessorKey";
 	}
 }

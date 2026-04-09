8280959de1c116645927f42e45992357dc748d00
Shawn Jackson <shawn@resgrid.com>
2026-04-08 19:24:28 -0700

    Update Web/Resgrid.Web/Areas/User/Models/Training/EditTrainingModel.cs
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- Web/Resgrid.Web/Areas/User/Models/Training/EditTrainingModel.cs
+++ Web/Resgrid.Web/Areas/User/Models/Training/EditTrainingModel.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using Resgrid.Model;
 
+using System.Collections.Generic;
+
 namespace Resgrid.Web.Areas.User.Models.Training
 {
 	public class EditTrainingModel
 	{
-		public Training Training { get; set; }
+		public Resgrid.Model.Training Training { get; set; }
 		public string Message { get; set; }
 		public bool SendToAll { get; set; }
 		public List<string> ExistingUserIds { get; set; }
 	}
+}
 }
